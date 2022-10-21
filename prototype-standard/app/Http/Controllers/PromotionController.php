0a0e3985124b3542477c512f297edaf727b0014a
Merradou Abderrahmane <abderahmane.merradou@gmail.com>
2022-10-21 16:43:19 +0100

    save

--- prototype-standard/app/Http/Controllers/PromotionController.php
+++ prototype-standard/app/Http/Controllers/PromotionController.php
@@ -3,8 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Promotion;
 
 class PromotionController extends Controller
 {
     //
+    public function index()
+    {
+        $promotions = Promotion::all();
+        return view('index', compact('promotions'));
+    }
+
+    public function create()
+    {
+        return view('addPromotion');
+    }
+
+    public function store(Request $request)
+    {
+        $promotion = new Promotion();
+        $promotion->promotionName = $request->promotionName;
+        $promotion->save();
+        // return redirect('/');
+        return redirect('index');
+    }
 }
